6d68641cfd4a9d908e1a3283414f2842bb3bbe30
Nitix <guillaume.pierson.gp@gmail.com>
2013-12-17 22:40:27 +0100

    Saved Database connection in a static attribute

--- Base.php
+++ Base.php
@@ -1,22 +1,26 @@
 <?php
 
-include('database.php');
-class Base
-{
-	static public function getConnection(){
-		try {
-			$database = new Database();
-			$dsn="mysql:host=$database->host;dbname=$database->database";
-			$db = new PDO($dsn, $database->user, $database->password, array(
-				PDO::ERRMODE_EXCEPTION=>true,
-				PDO::ATTR_PERSISTENT => true)
-			);
-			$db->exec("SET CHARACTER SET utf8"); 
+include ('database.php');
+class Base {
+	
+	public static $db;
+
+	static public function getConnection() {
+		if (isset($db)) {
 			return $db;
-		}catch(PDOException $e) {
-			echo $e->getMessage();
-			exit();
+		} else {
+			try {
+				$database = new Database();
+				$dsn = "mysql:host=$database->host;dbname=$database->database";
+				$db = new PDO($dsn, $database -> user, $database -> password, array(PDO::ATTR_ERRMODE => true, PDO::ERRMODE_EXCEPTION => true, PDO::ATTR_PERSISTENT => true));
+				$db -> exec("SET CHARACTER SET utf8");
+				return $db;
+			} catch(PDOException $e) {
+				echo $e -> getMessage();
+				exit();
+			}
 		}
+
 	}
+
 }
-		
