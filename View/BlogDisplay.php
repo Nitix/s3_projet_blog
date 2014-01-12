6c611713b1424c1b049dabbf2ce88c54583a72e7
Nitix <guillaume.pierson.gp@gmail.com>
2014-01-12 14:43:02 +0100

    Added a lot of messages if Categorie or Billet doesn't exist

--- View/BlogDisplay.php
+++ View/BlogDisplay.php
@@ -24,6 +24,7 @@ class BlogDisplay extends Display
 			<link rel="stylesheet" href="data/css/site.css"/>
 		</head>
 		<body>
+			<header>Bienvenue le BLOG<br />Ici on parle de tout et de rien, de rien surtout</header>
 			<div id="conteneur">'.$this->generateLeftMenu().$body.$this->generateRightMenu().'</div><br />
 			<footer>Ecris par Guillaume Pierson et Jordane Mahout</footer>
 		</body>
@@ -33,21 +34,30 @@ class BlogDisplay extends Display
 	private function billet()
 	{
 		$billet = $this->data;
-		$cat = Categorie::findById($billet->__get('cat_id'));
-		$html = '<section><article><h2>'.$billet->__get('titre').
-			'</h2><p>'.$billet->__get('body').'</p>Catégorie : <a href="Blog.php?a=cat&id='.$cat->__get('id').'">'.$cat->__get('titre').'</a></article></section>';
+		if(!empty($billet)){
+			$cat = Categorie::findById($billet->__get('cat_id'));
+			$html = '<section><article><h2>'.$billet->__get('titre').
+				'</h2><p>'.$billet->__get('body').'</p>Catégorie : <a href="Blog.php?a=cat&id='.$cat->__get('id').'">'.$cat->__get('titre').'</a></article></section>';
+		}else{
+			$html = '<section>Hmm, il semblerait que ce billet n\'existe pas, ou qu\'il a été supprimé, 
+				ou as-tu essayé de hacker, ou serait-ce une mauvaise programmation, je ne le saurai jamais, en tout cas il n\'y a pas le billet demandé.</section>';
+		}
 		return $html;
 	}
 	
 	private function listBillets()
 	{
-		$html = '<section><table><caption>Liste de tous les billets</caption><tr><th>Titre</th><th>Catégorie</th></tr>';
-		foreach ($this->data as $billet) {
-			$cat = Categorie::findById($billet->__get('cat_id'));
-			$html .= '<tr><td><a href="Blog.php?a=detail&id='.$billet->__get('id').'">'.$billet->__get('titre').
-			'</a></td><td><a href="Blog.php?a=cat&id='.$cat->__get('id').'">'.$cat->__get('titre').'</a></td></tr>';
+		if(!empty($this->data)){
+			$html = '<section><table><caption>Liste de tous les billets</caption><tr><th>Titre</th><th>Catégorie</th></tr>';
+			foreach ($this->data as $billet) {
+				$cat = Categorie::findById($billet->__get('cat_id'));
+				$html .= '<tr><td><a href="Blog.php?a=detail&id='.$billet->__get('id').'">'.$billet->__get('titre').
+				'</a></td><td><a href="Blog.php?a=cat&id='.$cat->__get('id').'">'.$cat->__get('titre').'</a></td></tr>';
+			}
+			$html .= '</table></section>';
+		}else{
+			$html = '<section>Il n\'y aucun billet pour l\'instant, hmm, si tu es admin, tu pourrais en créer, si tu ne l\'est pas reviens plus tard, je suis désolé :(</section>';
 		}
-		$html .= '</table></section>';
 		return $html;
 	}
 	
@@ -71,8 +81,12 @@ class BlogDisplay extends Display
 		$html = '<nav id="rightmenu">Liste des 10 derniers billets<br />';
 		try{
 			$billets = Billet::findLastLimited(10);
-			foreach ($billets as $billet) {
-					$html .= '<a href="Blog.php?a=detail&id='.$billet->__get('id').'">'.$billet->__get('titre').'</a><br />';
+			if(!empty($billets)){
+				foreach ($billets as $billet) {
+						$html .= '<a href="Blog.php?a=detail&id='.$billet->__get('id').'">'.$billet->__get('titre').'</a><br />';
+				}
+			}else{
+				$html = 'LOL, il n\'y a pas de billets...';
 			}
 		}catch(Exception $e){
 			$html .= 'Erreur à la récupération des catégories';
@@ -81,31 +95,43 @@ class BlogDisplay extends Display
 		return $html;
 	}
 	private function catDetail(){
-		$html = '<section><p>Liste de tous les billets dans la catégorie '.$this->data->__get('titre').'</p>Desciption : '.$this->data->__get('description').'<br /><ul>';
-		$billets = Billet::findByCat_ID($this->data->__get('id'));
-		if(!empty($billets)){
-			foreach ($billets as $billet) {
-				$html .= '<li><a href="Blog.php?a=detail&id='.$billet->__get('id').'">"'.$billet->__get('titre').'</a></li>';
+		if(!empty($this->data)){
+			$html = '<section><p>Liste de tous les billets dans la catégorie '.$this->data->__get('titre').'</p>Desciption : '.$this->data->__get('description').'<br />';
+			try{
+				$billets = Billet::findByCat_ID($this->data->__get('id'));
+				if(!empty($billets)){
+					$html .= '<ul>';
+					foreach ($billets as $billet) {
+						$html .= '<li><a href="Blog.php?a=detail&id='.$billet->__get('id').'">"'.$billet->__get('titre').'</a></li>';
+					}
+					$html .= '</ul></section>';
+				}else{
+					$html .= '<br /><div class="notFound">Il semblerait qu\'il n\'y aucun billet</div></section>';
+				}
+			}catch(Exception $e){
+				$html .= 'Erreur à la récupération des billets</section>';
 			}
-			$html .= '</ul></section>';
 		}else{
-			$html .= '<div class="notFound">Aucunde donnée trouvé</div>';
+			$html = 'Cette catégorie n\'existe pas, veuillez réssayer votre appel plus tard. Merci<br />Hein quoi? ce n\'est pas un appel, ah oups...</section>';
 		}
 		return $html;		
 	}
 	
-	private function allCat(){
-		$html = '<section><table><caption>Liste de toutes les catégories</caption><tr><th>Titre</th><th>Description</th></tr>';
-		foreach ($this->data as $billet) {
-			$cat = Categorie::findById($billet->__get('cat_id'));
-			$html .= '<tr><td><a href="Blog.php?a=cat&id='.$cat->__get('id').'">'.$cat->__get('titre').'</a></td>
-			<td>'.$cat->__get('description').'</a></td></td>';
+	private function listCats(){
+		if(!empty($this->data)){
+			$html = '<section><table><caption>Liste de toutes les catégories</caption><tr><th>Titre</th><th>Description</th></tr>';
+			foreach ($this->data as $cat) {
+				$html .= '<tr><td><a href="Blog.php?a=cat&id='.$cat->__get('id').'">'.$cat->__get('titre').'</a></td>
+				<td>'.$cat->__get('description').'</a></td></td>';
+			}
+			$html .= '</section>';
+		}else{
+			$html = '<section>Catégories pas exister, toi contacter Adminstrateur, toi gentil, toi merci</section>';
 		}
-		$html .= '</section>';
 		return $html;
 	}
 	
 	private function error(){
-		return $this->data;
+		return '<section><div class="error">$this->data</div></section>';
 	}
 }
