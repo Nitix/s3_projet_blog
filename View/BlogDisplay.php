fa312c0a9b3127beaae01fc68c9090b51b9a4c10
Nitix <guillaume.pierson.gp@gmail.com>
2014-01-13 17:01:11 +0100

    Use Display class for generic action

--- View/BlogDisplay.php
+++ View/BlogDisplay.php
@@ -61,39 +61,6 @@ class BlogDisplay extends Display
 		return $html;
 	}
 	
-	private function generateLeftMenu()
-	{
-		$html = '<nav id="leftmenu">Les Catégories<br />'; 
-		try{
-			$cats = Categorie::findAll();
-			foreach ($cats as $cat) {
-				$html .= '<a href="Blog.php?a=cat&amp;id='.$cat->__get('id').'">'.$cat->__get('titre').'</a><br />';
-			}
-		}catch(Exception $e){
-			$html .= 'Erreur à la récupération des catégories';
-		}
-		$html .= '</nav>';
-		return $html;
-	}
-	
-	private function generateRightMenu()
-	{
-		$html = '<nav id="rightmenu">Liste des 10 derniers billets<br />';
-		try{
-			$billets = Billet::findLastLimited(10);
-			if(!empty($billets)){
-				foreach ($billets as $billet) {
-						$html .= '<a href="Blog.php?a=detail&amp;id='.$billet->__get('id').'">'.$billet->__get('titre').'</a><br />';
-				}
-			}else{
-				$html = 'LOL, il n\'y a pas de billets...';
-			}
-		}catch(Exception $e){
-			$html .= 'Erreur à la récupération des catégories';
-		}
-		$html .= '</nav>';
-		return $html;
-	}
 	private function catDetail(){
 		if(!empty($this->data)){
 			$html = '<section><p>Liste de tous les billets dans la catégorie '.$this->data->__get('titre').'</p>Desciption : '.$this->data->__get('description').'<br />';
@@ -129,9 +96,5 @@ class BlogDisplay extends Display
 			$html = '<section>Catégories pas exister, toi contacter Adminstrateur, toi gentil, toi merci</section>';
 		}
 		return $html;
-	}
-	
-	private function error(){
-		return '<section><div class="error">$this->data</div></section>';
 	}
 }
